55d29fe6bf0f0896674c49d309baee56b2ee6735
Jim Hayes <jhayes@sdsc.edu>
2014-09-23 14:21:45 -0700

    Modulefile rpm build restructuring.
    Adopt/adapt Rocks approach to copying version from corresponding s/w package.
    Move all variant definitions into version.mk so that identical modulefile
    Makefile will work across rolls.

--- src/beast-modules/version.mk
+++ src/beast-modules/version.mk
@@ -1,6 +1,9 @@
-NAME       = beast-modules
-VERSION    = 1.8.0
-RELEASE    = 1
-PKGROOT    = /opt/modulefiles/applications/beast
+NAME        = beast-modules
+RELEASE     = 2
+PKGROOT     = /opt/modulefiles/applications/beast
 
-RPM.EXTRAS = AutoReq:No
+VERSION_SRC = $(REDHAT.ROOT)/src/beast_1.8.0/version.mk
+VERSION_INC = version.inc
+include $(VERSION_INC)
+
+RPM.EXTRAS  = AutoReq:No
