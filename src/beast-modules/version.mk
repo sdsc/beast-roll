d29c844fc86d57fe389bde406206caa74b1e08f4
Jim Hayes <jhayes@sdsc.edu>
2014-09-30 13:17:05 -0700

    Use canonical modulefile/roll-test build files.

--- src/beast-modules/version.mk
+++ src/beast-modules/version.mk
@@ -1,8 +1,11 @@
-NAME        = beast-modules
-RELEASE     = 2
-PKGROOT     = /opt/modulefiles/applications/beast
+PACKAGE     = beast
+CATEGORY    = applications
 
-VERSION_SRC = $(REDHAT.ROOT)/src/beast_1.8.0/version.mk
+NAME        = $(PACKAGE)-modules
+RELEASE     = 2
+PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
+
+VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)_1.8.0/version.mk
 VERSION_INC = version.inc
 include $(VERSION_INC)
 
